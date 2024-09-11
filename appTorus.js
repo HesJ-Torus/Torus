ab3afd03760d56c417f6225449b35b86f4c5a7d2
HesJ-Torus <Harryspencerjones@hotmail.co.uk>
2024-09-11 11:41:23 +0100

    Add files via upload

---
+++ appTorus.js
@@ -0,0 +1,28 @@
+let timeline = gsap.timeline();
+
+timeline.to(".imageWrap",{
+	height: "500px",
+	width: "21%",
+	duration: 1,
+	ease: "power4.Out",
+}).to(".imageWrap",{
+	height: "300px",
+	width: "10%",
+	duration: 1,
+	y: "0",
+	ease: "power4.Out",
+}, 1).from(".bigName", {
+	y: getYDistance(".bigName"),
+	duration: 1,
+}, 1).from(".hide",{
+	opacity: "0", 
+	duration: 1,
+}, 1);
+
+
+function getYDistance(el) {
+	return(
+	window.innerHeight - document.querySelector(el).getBoundingClientRect().
+	top
+	);
+}
