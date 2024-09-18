f3be32e30a280e116e81e6b566bfc626f746bf56
HesJ-Torus <Harryspencerjones@hotmail.co.uk>
2024-09-18 22:55:15 +0100

    sfd

--- appTorus.js
+++ appTorus.js
@@ -85,43 +85,103 @@ gsap.from(".contentHead",{
 		toggleActions:"restart none reverse none"
 	},
 	opacity:"0",
-	fontSize: "6rem",
-	
-});
+	fontSize: "6rem",}
+);
 
-gsap.to(".content1", {
+gsap.from(".textLeft",{
+	scrollTrigger:{
+		trigger:".textLeft",
+		start:"0 80%",
+		end:"+=0 10%",
+		scrub:1,
+		toggleActions:"restart none reverse none"
+	},
+	opacity:"0",
+	fontSize: "2rem",}
+);
+
+gsap.from(".content1", {
 	scrollTrigger:{
 		trigger:".content1",
-		markers:true,
-		start:"30px 65%",
-		end:"+=0 50%",
+		start:"100px 80%",
+		end:"+=0 10%",
 		scrub:2,
 		toggleActions:"restart none reverse none",
 	},
-	width:"100%"
+	width:"70%",
+	opacity: "0"
 });
 
+gsap.from(".contentHead2",{
+	scrollTrigger:{
+		trigger:".contentHead2",
+		start:"0 80%",
+		end:"+=0 10%",
+		scrub:1,
+		toggleActions:"restart none reverse none"
+	},
+	opacity:"0",
+	fontSize: "6rem",}
+);
 
+gsap.from(".textRight",{
+	scrollTrigger:{
+		trigger:".textRight",
+		start:"0 80%",
+		end:"+=0 10%",
+		scrub:1,
+		toggleActions:"restart none reverse none"
+	},
+	opacity:"0",
+	fontSize: "2rem",}
+);
 
-gsap.to(".content2", {
+gsap.from(".content2", {
 	scrollTrigger:{
 		trigger:".content2",
-		start:"center 65%",
-		end:"+=0 50%",
+		start:"100px 80%",
+		end:"+=0 10%",
 		scrub:2,
 		toggleActions:"restart none reverse none"
 	},
-	width:"100%",
+	width:"70%",
+	opacity: "0"
 });
-gsap.to(".content3", {
+
+gsap.from(".contentHead3",{
+	scrollTrigger:{
+		trigger:".contentHead3",
+		start:"0 80%",
+		end:"+=0 10%",
+		scrub:1,
+		toggleActions:"restart none reverse none"
+	},
+	opacity:"0",
+	fontSize: "6rem",}
+);
+
+gsap.from(".textRight2",{
+	scrollTrigger:{
+		trigger:".textRight2",
+		start:"0 80%",
+		end:"+=0 10%",
+		scrub:1,
+		toggleActions:"restart none reverse none"
+	},
+	opacity:"0",
+	fontSize: "2rem",}
+);
+
+gsap.from(".content3", {
 	scrollTrigger:{
 		trigger:".content3",
-		start:"center 65%",
-		end:"+=0 50%",
+		start:"100px 80%",
+		end:"+=0 10%",
 		scrub:2,
 		toggleActions:"restart none reverse none"
 	},
-	width:"100%",
+	width:"70%",
+	opacity: "0"
 });
 
 
