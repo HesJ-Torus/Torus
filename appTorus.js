ec9c3e8fc949918a2ffa83f19bd5a929a3a306f6
HesJ-Torus <Harryspencerjones@hotmail.co.uk>
2024-09-20 15:16:36 +0100

    finally

--- appTorus.js
+++ appTorus.js
@@ -76,120 +76,89 @@ gsap.to(".imageWrap", {
 	bottom:"100%",
 });
 
-gsap.from(".contentHead",{
+gsap.from(".titanium",{
 	scrollTrigger:{
-		trigger:".content",
-		start:"0 80%",
-		end:"+=0 40%",
-		scrub:2,
-		toggleActions:"restart none reverse none",
+		trigger:".titanium",
+		start:"top 100%",
+		end:"bottom 80%",
 		
+		scrub:1,
 	},
-	opacity:"0",
-	fontSize: "6rem",}
-);
-
-gsap.from(".textLeft",{
-	scrollTrigger:{
-		trigger:".textLeft",
-		start:"0 80%",
-		end:"+=0 40%",
-		scrub:2,
-		toggleActions:"restart none reverse none",
-	},
-	opacity:"0",
-	fontSize: "2rem",}
-);
-
-gsap.from(".content1", {
-	scrollTrigger:{
-		trigger:".content1",
-		start:"100px 80%",
-		end:"+=0 60%",
-		scrub:2,
-		toggleActions:"restart none reverse none",
-	},
-	width:"70%",
-	opacity: "0"
+	fontSize:"4rem",
 });
 
-gsap.from(".contentHead2",{
+gsap.from(".titaniumText",{
 	scrollTrigger:{
-		trigger:".contentHead2",
-		start:"0 80%",
-		end:"+=0 10%",
+		trigger:".titaniumText",
+		start:"top 100%",
+		end:"bottom 80%",
+		
 		scrub:1,
-		toggleActions:"restart none reverse none",
 	},
-	opacity:"0",
-	fontSize: "6rem",}
-);
+	fontSize:"2rem",
+})
 
-gsap.from(".textRight",{
+gsap.from(".section1Text",{
 	scrollTrigger:{
-		trigger:".textRight",
-		start:"0 80%",
-		end:"+=0 10%",
+		trigger:".section1Text",
+		start:"top 100%",
+		end:"bottom 80%",
+		
 		scrub:1,
-		toggleActions:"restart none reverse none",
 	},
-	opacity:"0",
-	fontSize: "2rem",}
-);
-
-gsap.from(".content2", {
-	scrollTrigger:{
-		trigger:".content2",
-		start:"100px 80%",
-		end:"+=0 10%",
-		scrub:2,
-		toggleActions:"restart none reverse none",
-	},
-	width:"70%",
-	opacity: "0"
+	fontSize:"1.5rem",
 });
 
-gsap.from(".contentHead3",{
+gsap.from(".section1Image",{
 	scrollTrigger:{
-		trigger:".contentHead3",
-		start:"0 80%",
-		end:"+=0 10%",
-		scrub:1,
-		toggleActions:"restart none reverse none",
-	},
-	opacity:"0",
-	fontSize: "6rem",}
-);
-
-gsap.from(".textRight2",{
-	scrollTrigger:{
-		trigger:".textRight2",
-		start:"0 80%",
-		end:"+=0 10%",
-		scrub:1,
-		toggleActions:"restart none reverse none",
-	},
-	opacity:"0",
-	fontSize: "2rem",}
-);
-
-gsap.from(".content3", {
-	scrollTrigger:{
-		trigger:".content3",
-		start:"100px 80%",
-		end:"+=0 10%",
+		trigger:".section1Text",
+		start:"top 100%",
+		end:"bottom 80%",
 		scrub:2,
-		toggleActions:"restart none reverse none",
 	},
-	width:"70%",
-	opacity: "0",
+	x:1000,
 });
 
+gsap.from(".section2Text",{
+	scrollTrigger:{
+		trigger:".section2Text",
+		start:"top 100%",
+		end:"bottom 80%",
+		scrub:1,
+	},
+	fontSize:"1.5rem",
+});
 
+gsap.from(".section2Image",{
+	scrollTrigger:{
+		trigger:".section2Text",
+		start:"top 100%",
+		end:"bottom 80%",
+		scrub:2,
+	},
+	x:-1000,
+});
 
+gsap.from(".section3Text",{
+	scrollTrigger:{
+		trigger:".section3Text",
+		start:"top 100%",
+		end:"bottom 80%",
+		scrub:1,
+	},
+	fontSize:"1.5rem",
+});
 
-
-
+gsap.from(".section3Image",{
+	scrollTrigger:{
+		trigger:".section3Text",
+		start:"top 80%",
+		end:"bottom 60%",
+		markers:true,
+		scrub:3,
+	},
+	y:1000,
+});
 
 
 function getYDistance(el) {
